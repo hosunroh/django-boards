878f2e3c338c5f0adefb54a7c93142f31d6541ef
hosunroh <hosunroh@gmail.com>
2022-03-19 19:59:07 +0900

    Add requirements.txt file

--- myproject/settings.py
+++ myproject/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import dj_database_url
+from decouple import config, Csv
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ciczuq87__(_d7-!+ij20jt&bameihk!fv=2&h+=^n5*_5rxm7'
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
+
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
